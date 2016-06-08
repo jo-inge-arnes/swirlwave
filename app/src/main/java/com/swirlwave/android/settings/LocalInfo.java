755eea6341a4dc93a848846af7c6fce04630b9cf
Jo Inge Arnes <mimsypimsy@outlook.com>
2016-06-08 12:12:54 +0200

    Added preferences value for application UUID

--- app/src/main/java/com/swirlwave/android/settings/LocalInfo.java
+++ app/src/main/java/com/swirlwave/android/settings/LocalInfo.java
@@ -7,19 +7,40 @@ import android.content.SharedPreferences;
 import java.util.UUID;
 
 public class LocalInfo {
-    private final String APP_PREFS = "SwirlwavePreferences";
-    private final String APP_ID = "SwirlwavePreferencesUuid";
+    private static final String APP_PREFS = "SwirlwavePreferences";
+    private static final String APP_PREFS_INITIALIZED = "PreferencesInitialized";
+    private static final String APP_ID_MOST_SIGNIFICANT_BITS = "IdMostSignificantBits";
+    private static final String APP_ID_LEAST_SIGNIFICANT_BITS = "IdLeastSignificant";
+    
     private SharedPreferences mSharedPreferences;
+    private UUID mId;
 
     public LocalInfo(Context context) {
         mSharedPreferences = context.getSharedPreferences(APP_PREFS, Activity.MODE_PRIVATE);
+        ensurePreferencesExist();
     }
 
     public void ensurePreferencesExist() {
-
+        if (!mSharedPreferences.getBoolean(APP_PREFS_INITIALIZED, false)) {
+            SharedPreferences.Editor editor = mySharedPreferences.edit();
+            
+            UUID uuid = UUID.randomUUID();
+            editor.putLong(APP_ID_MOST_SIGNIFICANT_BITS, uuid.getMostSignificantBits());
+            editor.putLong(APP_ID_LEAST_SIGNIFICANT_BITS, uuid.getLeastSignificantBits());
+            editor.putBoolean(APP_PREFS_INITIALIZED, true);
+            editor.apply();
+            
+            mId = uuid;
+        }
     }
 
     public UUID getId() {
-        return mSharedPreferences.getString(APP_ID, )
+        if (mId == null) {
+            mId = new UUID(
+                mSharedPreferences.getLong(APP_ID_MOST_SIGNIFICANT_BITS, 0L),
+                mSharedPreferences.getLong(APP_ID_LEAST_SIGNIFICANT_BITS, 0L));
+        }
+        
+        return mId;
     }
 }
