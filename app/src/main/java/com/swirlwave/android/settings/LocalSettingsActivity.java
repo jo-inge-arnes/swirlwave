4cdc2631d8ef1d30bb8d46a57d3192eca4c4f3d9
mr-speak <jo.inge.arnes@live.no>
2016-09-28 20:08:56 +0200

    Started on an activity for setting phone number, name and other local settings

---
+++ app/src/main/java/com/swirlwave/android/settings/LocalSettingsActivity.java
@@ -0,0 +1,13 @@
+package com.swirlwave.android.settings;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LocalSettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_local_settings);
+    }
+}
