cef5a453467cef186ffc153ee787e8acb18276c1
mr-speak <mimsypimsy@outlook.com>
2017-02-08 17:08:24 +0100

    added some tests for peers db

--- app/src/androidTest/java/com/swirlwave/android/peers/PeersDbTest.java
+++ app/src/androidTest/java/com/swirlwave/android/peers/PeersDbTest.java
@@ -11,6 +11,7 @@ import org.junit.runner.RunWith;
 
 import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
 @RunWith(AndroidJUnit4.class)
@@ -28,5 +29,46 @@ public class PeersDbTest {
         Peer peer = new Peer("test", uuid, "some-public-key", "+4798360000", "some-onion-address");
         long dbId = PeersDb.insert(mMockContext, peer);
         assertNotEquals(-1, dbId);
+    }
+
+    @Test
+    public void when_selecting_by_uuid_then_correct_peer_should_be_retrieved() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Peer peer = new Peer("test", uuid, "some-public-key", "+4798360000", "some-onion-address");
+        long dbId = PeersDb.insert(mMockContext, peer);
+
+        Peer returnedPeer = PeersDb.selectByUuid(mMockContext, uuid);
+        assertEquals(dbId, returnedPeer.getId());
+    }
+
+    @Test
+    public void when_updating_then_correct_peer_should_be_updated() throws Exception {
+        UUID uuid = UUID.randomUUID();
+        Peer peer = new Peer("test", uuid, "some-public-key", "+4798360000", "some-onion-address");
+        long dbId = PeersDb.insert(mMockContext, peer);
+
+        UUID uuid2 = UUID.randomUUID();
+        Peer peer2 = new Peer("test2", uuid2, "some-public-key", "+4798360000", "some-onion-address");
+        long dbId2 = PeersDb.insert(mMockContext, peer2);
+
+        peer2.setName("name_updated_ok");
+        peer2.setPublicKey("key_updated_ok");
+        peer2.setLastKnownAddress("address_updated_ok");
+        peer2.setPhoneNumber("phone_updated_ok");
+        PeersDb.update(mMockContext, peer2);
+
+        Peer selectedPeer = PeersDb.selectByUuid(mMockContext, uuid);
+        assertEquals(dbId, peer.getId());
+        assertEquals(peer.getName(), selectedPeer.getName());
+        assertEquals(peer.getPublicKey(), selectedPeer.getPublicKey());
+        assertEquals(peer.getPhoneNumber(), selectedPeer.getPhoneNumber());
+        assertEquals(peer.getLastKnownAddress(), selectedPeer.getLastKnownAddress());
+
+        selectedPeer = PeersDb.selectByUuid(mMockContext, uuid2);
+        assertEquals(dbId2, selectedPeer.getId());
+        assertEquals("name_updated_ok", selectedPeer.getName());
+        assertEquals("key_updated_ok", selectedPeer.getPublicKey());
+        assertEquals("phone_updated_ok", selectedPeer.getPhoneNumber());
+        assertEquals("address_updated_ok", selectedPeer.getLastKnownAddress());
     }
 }
