1482e0f51dd6fad56df12d96d45db44269578561
mr-speak <mimsypimsy@outlook.com>
2017-06-13 21:27:18 +0200

    added functionality for sending sms by tapping on peer in list

--- app/src/main/java/com/swirlwave/android/peers/PeersFragment.java
+++ app/src/main/java/com/swirlwave/android/peers/PeersFragment.java
@@ -8,24 +8,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.swirlwave.android.R;
+import com.swirlwave.android.sms.SmsSender;
+import com.swirlwave.android.toast.Toaster;
+
+import java.util.UUID;
 
 public class PeersFragment extends Fragment {
     private PeersFragmentAdapter mPeersFragmentAdapter;
+    private Context mContext;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.peer_list, container, false);
 
-        Context context = getActivity();
+        mContext = getActivity();
 
-        Cursor cursor = PeersDb.selectAll(context);
-        mPeersFragmentAdapter = new PeersFragmentAdapter(context, cursor);
+        Cursor cursor = PeersDb.selectAll(mContext);
+        mPeersFragmentAdapter = new PeersFragmentAdapter(mContext, cursor);
 
         ListView listView = (ListView)rootView.findViewById(R.id.peersListView);
         listView.setAdapter(mPeersFragmentAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Cursor cursor = (Cursor) mPeersFragmentAdapter.getItem(position);
+                int friendIdColumnIndex = cursor.getColumnIndex(PeersDb.PEER_ID_COLUMN);
+
+                UUID friendId = UUID.fromString(cursor.getString(friendIdColumnIndex));
+                Peer friend = PeersDb.selectByUuid(mContext, friendId);
+                PeersDb.updateOnlineStatus(mContext, friend, true);
+                Toaster.show(mContext, mContext.getString(R.string.sending_sms_to) + " " + friend.getName());
+                new Thread(new SmsSender(mContext, friendId)).start();
+            }
+        });
 
         return rootView;
     }
